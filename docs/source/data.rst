ed4ffe23a1a7127684def59585efed0885568a0e
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2024-11-06 12:26:59 -0500

    adding first data script

---
+++ docs/source/data.rst
@@ -0,0 +1,15 @@
+ftg.data
+====================
+
+.. contents:: Contents
+    :local:
+
+Data Objects
+------------
+
+.. currentmodule:: torch_geometric.data
+
+.. autosummary::
+   :nosignatures:
+   :toctree: ../generated
+   :template: autosummary/inherited_class.rst
---
+++ ftg/data.py
@@ -0,0 +1,45 @@
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    NamedTuple,
+    Optional,
+    Tuple,
+    Union,
+)
+
+
+class BaseData:
+    def __getattr__(self, key: str) -> Any:
+        raise NotImplementedError
+
+    def __setattr__(self, key: str, value: Any):
+        raise NotImplementedError
+
+    def __delattr__(self, key: str):
+        raise NotImplementedError
+
+    def __getitem__(self, key: str) -> Any:
+        raise NotImplementedError
+
+    def __setitem__(self, key: str, value: Any):
+        raise NotImplementedError
+
+    def __delitem__(self, key: str):
+        raise NotImplementedError
+    
+    def __copy__(self):
+        raise NotImplementedError
+
+    def __deepcopy__(self, memo):
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
+        raise NotImplementedError
+
+    
+
+    
+    
